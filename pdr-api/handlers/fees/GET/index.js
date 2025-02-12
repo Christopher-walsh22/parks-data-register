c162218526109cce4ca9fc40bf6833f66a77df8e
Christopher-walsh22 <106549296+Christopher-walsh22@users.noreply.github.com>
2025-02-12 19:08:57 +0000

    Add FeeGet Lambda function and FeeLoader tool

---
+++ pdr-api/handlers/fees/GET/index.js
@@ -0,0 +1,153 @@
+const { runQuery, runScan, TABLE_NAME} = require('/opt/dynamodb');
+const { sendResponse, logger } = require('/opt/base');
+
+
+exports.handler = async (event, context) => {
+  logger.debug('Get all fees for specific park', event);
+  // Allow CORS
+  if (event.httpMethod === 'OPTIONS') {
+    return sendResponse(200, {}, 'Success', null, context);
+  }
+
+  try {
+    const queryParams = event.queryStringParameters;
+    const queryType = getQueryType(queryParams);
+
+    let query;
+    if(queryParams){
+      switch (queryType) {
+        case 'withBilling':
+          if (!queryParams.billingPer || !queryParams.activity || !queryParams.facilityName || !queryParams.orc) {
+            throw {
+              code: 400,
+              error: 'Insufficient parameters.',
+              msg: `Missing required parameters for 'withBilling' query`
+            };
+          }
+          query = queryFeeWithBilling(queryParams.billingPer, queryParams.activity, queryParams.facilityName, queryParams.orc);
+          break;
+        case 'byActivity':
+          if (!queryParams.activity || !queryParams.facilityName || !queryParams.orc) {
+            throw {
+              code: 400,
+              error: 'Insufficient parameters.',
+              msg: `Missing required parameters for 'byActivity' query`
+            };
+          }
+          query = queryFeeByActivity(queryParams.activity, queryParams.facilityName, queryParams.orc);
+          break;
+        case 'byFacilityName':
+          if (!queryParams.facilityName || !queryParams.orc) {
+            throw {
+              code: 400,
+              error: 'Insufficient parameters.',
+              msg: `Missing required parameters for 'byFacilityName' query`
+            };
+          }
+          query = queryFeeByFacilityName(queryParams.facilityName, queryParams.orc);
+          break;
+        case 'byOrc':
+          if (!queryParams.orc) {
+            throw {
+              code: 400,
+              error: 'Insufficient parameters.',
+              msg: `Missing required query parameter: 'orc'`
+            };
+          }
+          query = queryFeeByOrc(queryParams.orc);
+          console.log("query:", query);
+          break;
+        default:
+          throw {
+            code: 400,
+            error: 'Insufficient parameters.',
+            msg: `Missing required query parameter: 'orc'`
+          };
+      }
+
+      let res = await runQuery(query);
+      console.log("res:", res);
+      return sendResponse(200, res, 'Success', null, context);
+    }
+    else {
+      let scan = await scanFees();
+      let res = await runScan(scan);
+      console.log("res:", res);
+      return sendResponse(200, res, 'Success', null, context);
+    }
+  } catch (err) {
+    logger.error(err);
+    return sendResponse(err?.code || 400, [], err?.msg || 'Error', err?.error || err, context);
+  }
+};
+
+function getQueryType(queryParams) {
+  if (queryParams?.billingPer) {
+    return 'withBilling';
+  } else if (queryParams?.activity) {
+    return 'byActivity';
+  } else if (queryParams?.facilityName) {
+    return 'byFacilityName';
+  } else if (queryParams?.orc) {
+    return 'byOrc';
+  } else {
+    return 'insufficientParams';
+  }
+}
+  function queryFeeByOrc(orc) {
+    let query = {
+      TableName: TABLE_NAME,
+      KeyConditionExpression: 'pk = :pk',
+      ExpressionAttributeValues: {
+        ':pk': {S: `${orc}::FEES`}
+      }
+    };  
+    return query;
+  }
+
+  function queryFeeByFacilityName(facilityName, orc) {
+    let query = {
+      TableName: TABLE_NAME,
+      KeyConditionExpression: 'pk = :pk AND begins_with(sk, :sk)',
+      ExpressionAttributeValues: {
+        ':pk': { S: `${orc}::FEES` },
+        ':sk': { S: facilityName }
+      }
+    };
+    return query;
+  }
+
+function queryFeeByActivity(activity, facilityName, orc) {
+  let query = {
+    TableName: TABLE_NAME,
+    KeyConditionExpression: 'pk = :pk AND begins_with(sk, :sk)',
+    ExpressionAttributeValues: {
+      ':pk': { S: `${orc}::FEES` },
+      ':sk': { S: `${facilityName}::${activity}` }
+    }
+  };
+  return query;
+}
+
+function queryFeeWithBilling(billing, activity, facilityName, orc) {
+  let query = {
+    TableName: TABLE_NAME,
+    KeyConditionExpression: 'pk = :pk AND begins_with(sk, :sk)',
+    ExpressionAttributeValues: {
+      ':pk': { S: `${orc}::FEES` },
+      ':sk': { S: `${facilityName}::${activity}::${billing}` }
+    }
+  };
+  return query
+}
+
+async function scanFees() {
+  let scan = {
+    TableName: TABLE_NAME,
+    FilterExpression: 'contains(pk, :fees)',
+    ExpressionAttributeValues: {
+      ':fees': { S: '::FEES' }
+    }
+  };
+  return scan;
+}
---
+++ pdr-api/tools/FeeLoader.js
@@ -0,0 +1,136 @@
+const readXlsxFile = require('read-excel-file/node');
+const fs = require('fs');
+//const filePath = "./feereg.csv"
+const {
+    PutItemCommand,
+    DynamoDBClient
+  } = require('@aws-sdk/client-dynamodb');
+const {
+    marshall
+} = require ('@aws-sdk/util-dynamodb')
+const TABLE_NAME = process.env.TABLE_NAME || 'NameRegister';
+const REGION = 'ca-central-1'; 
+const IS_OFFLINE = "TRUE"
+const DYNAMODB_ENDPOINT_URL = process.env.DYNAMODB_ENDPOINT_URL || 'http://172.17.0.2:8000';
+const options = {
+    region: REGION,
+    endpoint: DYNAMODB_ENDPOINT_URL
+  };
+
+
+  if (IS_OFFLINE === 'True') {
+    options.endpoint = 'http://172.17.0.2:8000';
+  }
+  const dynamoClient = new DynamoDBClient(options);
+let errorList = [];
+let errorCount = 0;
+const schema = {
+  'PK': {
+    prop: 'PK',
+    type: String
+  },
+  'SK': {
+    prop: 'SK',
+    type: String
+  },
+  'Night': {
+    prop: 'Night',
+    type: String
+  },
+  'Day': {
+    prop: 'Day',
+    type: String
+  },
+  'Use': {
+    prop: 'Use',
+    type: String
+  },
+  'Week': {
+    prop: 'Week',
+    type: String
+  },
+  'Year': {
+    prop: 'Year',
+    type: String
+  },
+  'Trip': {
+    prop: 'Trip',
+    type: String
+  },
+  'Direction of Trip': {
+    prop: 'Direction of Trip',
+    type: String
+  },
+  '28 Days': {
+    prop: '28 Days',
+    type: String
+  }
+}
+
+async function doMigration(filePath) {
+ 
+  console.log('path:', process.cwd())
+  console.log('filePath:', filePath);
+  try{
+
+
+
+
+    let { rows, errors } = await readXlsxFile(filePath, { schema, sheet: 'feereg' });
+
+    for (const row of rows) {
+      process.stdout.write('.');
+
+      // 1. Add the fee record
+      let feeRecord ={
+          pk: row['PK'],
+          sk: row['SK']
+      }
+
+      const availableFields = Object.keys(row);
+      const validFeeFields = ['Night', 'Day', 'Use', 'Week', 'Year', 'Trip', 'Direction of Trip', '28 Days'];
+
+      validFeeFields.forEach((field) => {
+          if (availableFields.includes(field) && row[field] !== undefined) {
+              feeRecord[field.replace(/\s+/g, '')] = row[field];
+          }
+      });
+
+      console.log("Processed Fee Record:", feeRecord);
+
+      if (!(await putItem(feeRecord))) {
+          console.log("There was an issue loading the fee for:", row[PK] + row[SK]);
+      }
+    }
+    return rows.length;
+  }catch(error){
+    console.log(error);
+  }
+  console.log("Errors: ", errorList)
+  console.log("Error Count: ", errorCount)
+}
+
+async function putItem(record) {
+  let putObject = {};
+    putObject = {
+      TableName: TABLE_NAME,
+      ConditionExpression: 'attribute_not_exists(pk) AND attribute_not_exists(sk)',
+      Item: marshall(record)
+    }
+  
+
+  try {
+    let putCommand = new PutItemCommand(putObject);
+    let res = await dynamoClient.send(putCommand);
+    return true;
+  } catch (err) {
+    console.log("There was an issue on line 128 loading fee: ", record.pk + record.sk)
+    console.log("Error: ", err)
+    errorList.push(`PK: ${record.pk} , SK:${record.sk} error: ${err}`)
+    errorCount++
+    return false;
+  }
+}
+
+doMigration('feereg2.xlsx')
+
